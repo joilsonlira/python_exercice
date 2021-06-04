8b8da6d87fa6963570a40c52f718190204780be2
joilsonlira <joilson.lira@live.com>
2021-06-04 11:51:01 -0300

    exercicios 1 e dois respondidos

---
+++ estrutura_sequencial/soma.py
@@ -0,0 +1,6 @@
+
+primeiro_valor = input("Primeiro valor: ")
+segundo_valor = input("Segundo valor: ")
+
+resultado_soma = primeiro_valor + segundo_valor
+print("A soma do valor",primeiro_valor, "com o valor",segundo_valor, "é",resultado_soma)
