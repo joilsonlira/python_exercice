57f203f66d681e3a532577c039f8ea20ee8a518d
Joilson Lira <lirajoilson@gmail.com>
2021-06-06 18:06:20 -0300

    iniciando resposta para numeros.py

--- estrutura_sequencial/numeros.py
+++ estrutura_sequencial/numeros.py
@@ -4,3 +4,10 @@ Faça um programa que peça 2 números inteiros e um número real. Calcule e mos
     b) a soma do triplo do primeiro com terceiro.
     c) o terceiro elevado ao cubo.
 """
+
+#Variaveis
+primeiro_inteiro = 0
+segundo_inteiro = 0
+numero_real = 0.0
+
+#
