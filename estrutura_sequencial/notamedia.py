002fd45f38f8f83394e1b795596202b21d9d6e44
Joilson Lira <lirajoilson@gmail.com>
2021-06-06 17:55:18 -0300

    resposta do exerciocio de media feito sem loop

--- estrutura_sequencial/notamedia.py
+++ estrutura_sequencial/notamedia.py
@@ -1,1 +1,20 @@
 #Faça um programa que peça as 4 notas bismestrais e mostre a média.
+
+#Variaveis.
+primeira_nota = 0
+segunda_nora = 0
+terceira_nora = 0
+quarta_nota = 0
+media = 0
+
+#solicitando notas ao usuario.
+primeira_nota = int(input("Primeira nota: "))
+segunda_nota = int(input("Segunda nota: "))
+terceira_nota = int(input("Terceira nota: "))
+quarta_nota = int(input("Quarta nota: "))
+
+#Calculo de media.
+media = (primeira_nota + segunda_nota + terceira_nota + quarta_nota) / 4
+
+#Exibindo media.
+print("Média: ", media)
