69b78bb92fd001f3c97c52021e406524dd7e5664
joilsonlira <joilson.lira@live.com>
2021-06-10 22:49:28 -0300

    modifiquei os nomes dos exercicios e fiz o exercicio 2

---
+++ estrutura_sequencial/ex_2_mostrar_numero.py
@@ -0,0 +1,9 @@
+#Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+#--Variaveis--
+numero = 0
+
+#--Solicitando numero--
+numero = input("Informe um número: ")
+
+#--Mostrando numero com mensagem--
+print("O número informado foi", numero)
