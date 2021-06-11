1fc5c456d0197b49367597756865cddb46ce3db4
joilsonlira <joilson.lira@live.com>
2021-06-10 23:26:57 -0300

    respondido exercicio 5

---
+++ estrutura_sequencial/ex_5_converter_metro_centimetros.py
@@ -0,0 +1,15 @@
+#Faça um Programa que converta metros para centímetros.
+
+#--variaveis--
+para_converter = 0
+convertido = 0
+
+#--solicitando valor--
+para_converter = float(input("me informe o valor em metros: "))
+
+#--convertendo--
+convertido = para_converter * 100
+
+#--Exibindo resultado convertido--
+print(convertido,"Cm")
+
